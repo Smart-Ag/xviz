d9b8a5b8347974509335e62dd5df755c348798ef
Rory Hubbard <calvin.hubbard@ravenind.com>
2021-05-27 11:34:14 -0500

    unimportant change

--- python/examples/tracking_prototype.py
+++ python/examples/tracking_prototype.py
@@ -5,6 +5,7 @@ from scenarios.utils.com_manager import ComManager, MqttConst
 from scenarios.safety_subsystems.radar_filter import SmartMicroRadarFilter
 from radar.read import get_target_sets
 from radar.tracker import Tracker
+from radar.tracks import TrackType
 
 
 def publish_tracks(comm, tracks):
@@ -24,7 +25,6 @@ def main():
     collector_instances = get_collector_instances(collector_output_file,
                                                   extract_directory)
 
-
     sm_filter = SmartMicroRadarFilter(dBpower_threshold=80.)
     raw_target_sets, filtered_target_sets = get_target_sets(collector_instances,
                                                             sm_filter)
@@ -36,7 +36,12 @@ def main():
     cost_threshold = 4
     updates_until_alive = 4
     kill_threshold = 4
-    tracker = Tracker(dt, updates_until_alive, cost_threshold, kill_threshold)
+    track_type = TrackType.smart_micro \
+        if collector_config['smartmicro_radar'] \
+        else TrackType.bosch
+
+    tracker = Tracker(dt, updates_until_alive,
+                      cost_threshold, kill_threshold, track_type)
 
     for target_set in filtered_target_sets:
         if not target_set or target_set is None:
